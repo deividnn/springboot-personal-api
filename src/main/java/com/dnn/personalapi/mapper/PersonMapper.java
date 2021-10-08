6ace27fd84951b550f061ccc2553f2e1748b3c6c
Deivid Nn <deividnn@users.noreply.github.com>
2021-10-08 17:37:04 -0300

    ok

--- src/main/java/com/dnn/personalapi/mapper/PersonMapper.java
+++ src/main/java/com/dnn/personalapi/mapper/PersonMapper.java
@@ -6,14 +6,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-import java.util.Map;
-
 @Mapper
 public interface PersonMapper {
 
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    @Mapping(target = "birthDate",source = "birthDate",dateFormat = "dd-MM-yyyy")
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO personDTO);
 
     PersonDTO toDTO(Person person);
